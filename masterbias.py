90d6560d3a64d4df56932c3f3bb998c25bbaf33d
Walter Martins-Filho <waltersmartinsf@gmail.com>
2016-02-05 16:56:49 -0200

    masterbias and ExoSetup from ExoDRPL in python

---
+++ masterbias.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 16:22:13 2016
+
+@author: walter
+"""
+
+from pyraf import iraf
+from astropy.io import fits
+from login import * #loading login.cl parameters for iraf
+from ExoSetupTaskParameters import *
